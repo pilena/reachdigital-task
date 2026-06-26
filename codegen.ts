dfc51b0baef74ea2e63d9af92d0f57095871e188
pilena <lenkazivkovic24@gmail.com>
2026-06-26 16:19:47 +0200

    feat: initial project setup

---
+++ codegen.ts
@@ -0,0 +1,16 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'https://backend.reachdigital.dev/graphql',
+  documents: 'src/**/*.graphql',
+  generates: {
+    'src/generated/graphql.ts': {
+      plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
+      config: {
+        withHooks: true,
+      },
+    },
+  },
+};
+
+export default config;
---
+++ src/lib/apolloClient.ts
@@ -0,0 +1,6 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+export const apolloClient = new ApolloClient({
+  uri: 'https://backend.reachdigital.dev/graphql',
+  cache: new InMemoryCache(),
+});
