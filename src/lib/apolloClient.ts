a6f91fa523bd72a52bcc007a8e79f3ee7d6f0258
pilena <lenkazivkovic24@gmail.com>
2026-06-26 16:56:58 +0200

    fix: update apolloClient configuration to use HttpLink

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,6 +1,8 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
-  uri: "https://backend.reachdigital.dev/graphql",
+  link: new HttpLink({
+    uri: 'https://backend.reachdigital.dev/graphql',
+  }),
   cache: new InMemoryCache(),
 });
