12b9ba3943e353cdde7ca14f02fb3998404e8fef
pilena <lenkazivkovic24@gmail.com>
2026-06-28 19:02:45 +0200

    fix: use HttpLink instead of deprecated uri option in Apollo Client

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,8 +1,8 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://backend.reachdigital.dev/graphql',
+    uri: "https://backend.reachdigital.dev/graphql",
   }),
   cache: new InMemoryCache(),
 });
